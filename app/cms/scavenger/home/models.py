9c2726c0837d903a5b9e0afd6bb3b84cc2a3f0cd
Michael Hardin <mhardinla@gmail.com>
2022-02-08 21:37:04 -0800

    added homepage spotlight post

--- app/cms/scavenger/home/models.py
+++ app/cms/scavenger/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel
 
 from wagtail.core.models import Page
 
@@ -6,11 +7,24 @@ from posts.models import Post
 
 
 class HomePage(Page):
-    pass
+    spotlight_post = models.ForeignKey(
+        "posts.Post",
+        null=True,
+        blank=True,
+        on_delete=models.PROTECT,
+        related_name="spotlight_parent",
+    )
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
         context["posts"] = (
-            Post.objects.child_of(self).live().order_by("-first_published_at")
+            Post.objects.child_of(self)
+            .live()
+            .filter(spotlight_parent__isnull=True)
+            .order_by("-first_published_at")
         )
         return context
+
+    content_panels = Page.content_panels + [
+        FieldPanel("spotlight_post"),
+    ]
