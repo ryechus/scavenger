475d831bc662c01f79aa6ab99066033145745f60
MJ Hardin <mhardinla@gmail.com>
2022-03-19 01:49:01 -0700

    use postgres

--- app/cms/scavenger/scavenger/settings/production.py
+++ app/cms/scavenger/scavenger/settings/production.py
@@ -1,3 +1,13 @@
 from .base import *  # noqa
 
 DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
+        "HOST": os.environ.get("DB_HOST"),
+    }
+}
