1229c4abe48661ec369c4ae55643bbb544187937
Michael Hardin <mhardinla@gmail.com>
2022-03-23 18:59:07 -0700

    moved scripts

--- app/cms/scavenger/scripts/build.sh
+++ app/cms/scavenger/scripts/build.sh
@@ -2,6 +2,6 @@
 commit_sha=$(git rev-parse HEAD)
 repo_url="188863028714.dkr.ecr.us-west-1.amazonaws.com/scavenger_blog"
 
-docker build -t $repo_url:$commit_sha -f app/cms/scavenger/Minimal.Dockerfile app/cms/scavenger
+docker build -t $repo_url:$commit_sha -f Minimal.Dockerfile .
 
 AWS_PROFILE=zilla docker push $repo_url:$commit_sha
