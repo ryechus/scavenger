60fd129f7c614ac48a8c0bb479a04b594af1efed
Michael Hardin <mhardinla@gmail.com>
2022-03-23 17:13:15 -0700

    add defaults

--- app/cms/scavenger/scavenger/settings/storage_s3.py
+++ app/cms/scavenger/scavenger/settings/storage_s3.py
@@ -4,10 +4,16 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
 
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_S3_STORAGE_BUCKET")
+AWS_STORAGE_BUCKET_NAME = os.environ.get(
+    "AWS_S3_STORAGE_BUCKET", "scavenger-django-storage"
+)
 
 AWS_DEFAULT_ACL = "public-read"
 
-STATIC_URL = os.environ.get("AWS_S3_URL")
+MEDIA_ROOT = "/media/"
 
-MEDIA_URL = os.environ.get("AWS_S3_URL")
+STATIC_ROOT = "/static/"
+
+STATIC_URL = f'"{os.environ.get("AWS_S3_URL")}"/static'
+
+MEDIA_URL = f'"{os.environ.get("AWS_S3_URL")}"/media'
