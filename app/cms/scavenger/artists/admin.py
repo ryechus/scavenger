e844e5720f6bf8c29cef3cac329098a8aa646498
Michael Hardin <mhardinla@gmail.com>
2022-03-23 21:27:12 -0700

    use tag on artist page

--- app/cms/scavenger/artists/admin.py
+++ app/cms/scavenger/artists/admin.py
@@ -8,9 +8,11 @@ class ArtistsAdmin(ModelAdmin):
     menu_icon = "user"  # change as required
     menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
-    exclude_from_explorer = False  # or True to exclude pages of this type from Wagtail's explorer view
-    list_display = ("name",)
-    search_fields = ("name",)
+    exclude_from_explorer = (
+        False  # or True to exclude pages of this type from Wagtail's explorer view
+    )
+    list_display = ("tag",)
+    search_fields = ("tag",)
 
 
 # Now you just need to register your customised ModelAdmin class with Wagtail
