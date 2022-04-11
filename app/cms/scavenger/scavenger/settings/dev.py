d3fe9d1f64e88b3a223e9f737bd9952a8d145edb
MJ Hardin <mhardinla@gmail.com>
2022-04-10 23:21:05 -0700

    debug = False

--- app/cms/scavenger/scavenger/settings/dev.py
+++ app/cms/scavenger/scavenger/settings/dev.py
@@ -2,7 +2,7 @@ from .base import *  # noqa
 from .storage_s3 import *  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-$+!7w!%mireedhxlzi73do&fgz8+-3m9w*ch-r!0xvz^fu=r6@"
