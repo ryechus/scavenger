4047ecc26c7f7f6a4a8a74d9d61f017a551b6553
MJ Hardin <mhardinla@gmail.com>
2022-04-10 21:39:50 -0700

    use postgres

--- app/cms/scavenger/scavenger/settings/dev.py
+++ app/cms/scavenger/scavenger/settings/dev.py
@@ -24,3 +24,13 @@ LOGGING = {
         "level": "WARNING",
     },
 }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
+        "HOST": os.environ.get("DB_HOST"),
+    }
+}
