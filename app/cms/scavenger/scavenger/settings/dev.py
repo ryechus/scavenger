e6c77047e77800889bcce93479a28c33cbcf4042
MJ Hardin <mhardinla@gmail.com>
2022-04-13 21:42:19 -0700

    specify port

--- app/cms/scavenger/scavenger/settings/dev.py
+++ app/cms/scavenger/scavenger/settings/dev.py
@@ -33,5 +33,6 @@ DATABASES = {
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASS"),
         "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT")
     }
 }
