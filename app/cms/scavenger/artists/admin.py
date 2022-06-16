b37cbcd16a32ddba2450af0b98d555a52d3a2127
MJ Hardin <mhardinla@gmail.com>
2022-06-16 00:27:52 -0700

    added view posts to artist admin

--- app/cms/scavenger/artists/admin.py
+++ app/cms/scavenger/artists/admin.py
@@ -1,3 +1,6 @@
+from django.contrib import admin
+from django.urls import reverse_lazy
+from django.utils.safestring import mark_safe
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
 
 from .models import Artist
@@ -8,12 +11,15 @@ class ArtistsAdmin(ModelAdmin):
     menu_icon = "user"  # change as required
     menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
     add_to_settings_menu = False  # or True to add your model to the Settings sub-menu
-    exclude_from_explorer = (
-        False  # or True to exclude pages of this type from Wagtail's explorer view
-    )
-    list_display = ("tag",)
+    exclude_from_explorer = False  # or True to exclude pages of this type from Wagtail's explorer view
+    list_display = ("tag", "posts")
     search_fields = ("tag",)
 
+    @admin.display
+    def posts(self, obj):
+        path = reverse_lazy("posts:artist", args=[obj.tag.name])
+        html = f"<a href='{path}'>View Posts</a>"
+        return mark_safe(html)
 
-# Now you just need to register your customised ModelAdmin class with Wagtail
+
 modeladmin_register(ArtistsAdmin)
