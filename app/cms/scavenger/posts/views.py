8c3829b726eccb9a81a16d0c8de5ad38ba7951c2
MJ Hardin <mhardinla@gmail.com>
2022-06-15 23:40:41 -0700

    use iexact when filtering

--- app/cms/scavenger/posts/views.py
+++ app/cms/scavenger/posts/views.py
@@ -5,12 +5,12 @@ from posts.models import Post
 
 
 def get_posts_by_tag(request, tag):
-    posts = Post.objects.filter(tags__name=tag).order_by("-first_published_at")
+    posts = Post.objects.filter(tags__name__iexact=tag)
 
     return render(request, "posts/tags.html", context={"posts": posts, "tag": tag})
 
 
 def get_posts_by_artist_name(request, artist_name):
-    posts = Post.objects.filter(artists__name=artist_name)
+    posts = Post.objects.filter(artists__name__iexact=artist_name)
 
     return render(request, "posts/tags.html", context={"posts": posts, "tag": artist_name})
