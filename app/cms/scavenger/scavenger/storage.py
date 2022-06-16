6d11614cbf4080d9adbefc952ea0b5481b5be781
MJ Hardin <mhardinla@gmail.com>
2022-06-16 09:15:03 -0700

    use ManifestFilesMixin

--- app/cms/scavenger/scavenger/storage.py
+++ app/cms/scavenger/scavenger/storage.py
@@ -1,9 +1,10 @@
+from django.contrib.staticfiles.storage import ManifestFilesMixin
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticRootS3BotoStorage(S3Boto3Storage):  # noqa
+class StaticRootS3BotoStorage(ManifestFilesMixin, S3Boto3Storage):  # noqa
     location = "static"
 
 
-class MediaRootS3BotoStorage(S3Boto3Storage):  # noqa
+class MediaRootS3BotoStorage(ManifestFilesMixin, S3Boto3Storage):  # noqa
     location = "media"
