25a4ae0b8211c6df9e7dea082ed463328d1060a6
MJ Hardin <mhardinla@gmail.com>
2022-06-17 22:40:14 -0700

    added max_entries

--- app/cms/scavenger/scavenger/settings/cache.py
+++ app/cms/scavenger/scavenger/settings/cache.py
@@ -6,5 +6,7 @@ CACHES = {
     "renditions": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": ["redis://scavenger-redis-master:6379", "redis://scavenger-redis-replicas:6379"],
+        "TIMEOUT": 86_400,
+        "OPTIONS": {"MAX_ENTRIES": 1000},
     },
 }
