d24eee0c2e7c111c1af97262093ae9c1b92c9b13
MJ Hardin <mhardinla@gmail.com>
2022-06-17 22:07:32 -0700

    added pagination

--- app/cms/scavenger/home/models.py
+++ app/cms/scavenger/home/models.py
@@ -1,3 +1,4 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db import models
 from posts.models import Post
 from wagtail.admin.edit_handlers import FieldPanel
@@ -15,12 +16,25 @@ class HomePage(Page):
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
-        context["posts"] = (
+        posts = (
             Post.objects.child_of(self)
             .live()
             .filter(spotlight_parent__isnull=True)
             .order_by("-first_published_at")
         )
+        paginator = Paginator(posts, 25)
+
+        page = request.GET.get("page")
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            posts = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            posts = paginator.page(paginator.num_pages)
+
+        context["posts"] = posts
         return context
 
     content_panels = Page.content_panels + [
