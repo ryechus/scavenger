5fca92524dbfa54d14583fb7a805a6dcba7072e4
MJ Hardin <mhardinla@gmail.com>
2022-06-17 21:36:54 -0700

    add staff_member_required decorator

--- app/cms/scavenger/scavenger/views.py
+++ app/cms/scavenger/scavenger/views.py
@@ -1,5 +1,6 @@
 import os
 
+from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponse
 
 
@@ -7,5 +8,6 @@ def health_check(request):
     return HttpResponse()
 
 
+@staff_member_required
 def source_code_version(request):
     return HttpResponse(os.environ.get("SOURCE_CODE_VERSION", "not set").encode())
