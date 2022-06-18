e26b40df44df983eec6400f21c9b94f4400952b3
MJ Hardin <mhardinla@gmail.com>
2022-06-17 22:56:00 -0700

    prefetch image renditions

--- app/cms/scavenger/home/models.py
+++ app/cms/scavenger/home/models.py
@@ -1,8 +1,10 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db import models
+from django.db.models import Prefetch
 from posts.models import Post
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.models import Page
+from wagtail.images import get_image_model
 
 
 class HomePage(Page):
@@ -16,10 +18,15 @@ class HomePage(Page):
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
+        renditions_queryset = (
+            get_image_model().get_rendition_model().objects.filter(filter_spec__in=["fill-640x640"])
+        )
         posts = (
             Post.objects.child_of(self)
             .live()
             .filter(spotlight_parent__isnull=True)
+            .select_related("post_images")
+            .prefetch_related(Prefetch("post_images__renditions", queryset=renditions_queryset))
             .order_by("-first_published_at")
         )
         paginator = Paginator(posts, 25)
