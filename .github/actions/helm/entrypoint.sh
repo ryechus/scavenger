7305b409a7fd68422776d91602427a2d6bdad8c1
MJ Hardin <mhardinla@gmail.com>
2022-06-25 11:27:00 -0700

    updated entrypoint to chmod /tmp/config

--- .github/actions/helm/entrypoint.sh
+++ .github/actions/helm/entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+chmod 600 /tmp/config
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
