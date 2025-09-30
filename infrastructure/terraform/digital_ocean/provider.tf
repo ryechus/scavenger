5fe460e34625a29a81c2cec51c9408c11421e5db
Mike Hardin <mhardinla@gmail.com>
2025-09-30 09:54:48 -0700

    removed sensitive credential

--- infrastructure/terraform/digital_ocean/provider.tf
+++ infrastructure/terraform/digital_ocean/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
@@ -17,7 +17,8 @@ terraform {
 # Set the variable value in *.tfvars file
 # or using -var="do_token=..." CLI option
 variable "do_token" {
-  default = "dop_v1_8e93ef76def94c241b8f1c7ad3fac48c200830ad2d98dd947434ae0af8c302c8"
+  type        = string
+  description = ""
 }
 
 # Configure the DigitalOcean Provider
